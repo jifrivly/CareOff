8c657c10c23b913814d3310100690e88418aadfe
mahakeemmk <dev.mahakeemmk@gmail.com>
2018-04-21 18:09:24 +0530

    modal for signin.

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -39,12 +39,33 @@
                 </form>
                 <ul class="nav justify-content-end">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">SignIn</a>
+                        <a class="nav-link" href="" data-toggle="modal" data-target="#signin">SignIn</a>
                     </li>  
                 </ul>
 
             </div>
         </nav>
+
+        <!-- Modal -->
+        <div class="modal fade" id="signin" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
 
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
